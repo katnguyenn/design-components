c586c7fe1837ab16445eb6314adaa7263cc38d26
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-18 16:55:27 -0700

    created first theme importing colors and typography to define button and text colors

---
+++ src/utils/themes.js
@@ -0,0 +1,11 @@
+import { blue, neutral } from "./colors";
+import { primaryFont } from "./typography";
+export const defaultTheme = {
+    primaryColor: blue[300],
+    primaryColorHover: blue[200],
+    primaryColorActive: blue[100],
+    textColorOnPrimary: neutral[100],
+    textColor: neutral[400],
+    textColorInverted: neutral[300],
+    primaryFont
+};
