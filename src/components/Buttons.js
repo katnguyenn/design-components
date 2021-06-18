2dd6aa8189fbd499fc9ac7e09ff8918680dd919e
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-18 16:22:55 -0700

    created Button using CSS-in-JS and imported primary color

---
+++ src/components/Buttons.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+const primaryBlue = "#2A4793"
+
+const Button = styled.button `
+    padding: 12px 24px;
+    font-size: 1rem;
+    border-radius: 2px;
+    min-width: 100px;
+    cursor: pointer;
+    font-family: "Roboto Mono", monospace;
+`
+
+const PrimaryButton = styled(Button) `
+    background-color: ${primaryBlue};
+    border: none;
+    color: white;
+   
+
+`;
+
+
+
+export default PrimaryButton;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import PrimaryButton from "./components/Buttons";
 
-const App = () => <h1>Hello World</h1>
+const App = () => <PrimaryButton>Hello World</PrimaryButton>
 
 ReactDOM.render(<App />, document.getElementById('root'));
