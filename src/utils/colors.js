3ad3fdc94913882f91e16684b7f41f6b2ccfe7e3
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-18 16:45:50 -0700

    added color palette

---
+++ src/utils/colors.js
@@ -0,0 +1,20 @@
+export const blue = {
+    100: "#3f66ca",
+    200: "#3354ab",
+    300: "#2A4793",
+    400: "#243e82",
+    500: "#1e346d"
+};
+
+export const orange = {
+    100: "#FFD7BA",
+    200: "#FEC89A",
+    300: "#fb8500"
+};
+
+export const neutral = {
+    100: "#F2F2F2",
+    200: "#CCCCCC",
+    300: "#A5A5A5",
+    
+};
