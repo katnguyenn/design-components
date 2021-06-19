485ec41d2088b6a74c315a5273a17933fbe8fd9e
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-06-19 11:56:04 -0700

    installed storybook

---
+++ .storybook/main.js
@@ -0,0 +1,19 @@
+module.exports = {
+  "stories": [
+    "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-docs", 
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true
+      }
+    }
+  ]
+}
